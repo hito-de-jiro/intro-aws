ca1ff2d44fce7618991e58cf3c52f0e13281de53
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-12-10 11:59:12 +0200

    style: correct code

--- aws-sdk/aws-sdk-1.py
+++ aws-sdk/aws-sdk-1.py
@@ -1,7 +1,7 @@
 import boto3
 
-S3_RESOURCE  = boto3.resource('s3')
+S3_RESOURCE = boto3.resource('s3')
 print('My S3 buckets:')
 
-for bucket in S3_RESOURCE .buckets.all():
+for bucket in S3_RESOURCE.buckets.all():
     print(f"\t- {bucket.name}")
